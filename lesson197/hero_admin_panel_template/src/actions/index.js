2423560dfd390d1fc4c79f632b646313b5aaf51f
Ivan Zviagin <ivan@ivan.pw>
2023-03-01 09:52:19 +0400

    lesson 206 - upgrade react-scripts, redux toolkit: createReducer()

--- lesson197/hero_admin_panel_template/src/actions/index.js
+++ lesson197/hero_admin_panel_template/src/actions/index.js
@@ -7,40 +7,15 @@ export const fetchHeroes = (request) => (dispatch) => {
     .catch(() => dispatch(heroesFetchingError()));
 };
 
-// export const heroesFetching = () => {
-//   return {
-//     type: 'HEROES_FETCHING',
-//   };
-// };
-
 export const heroesFetching = createAction('HEROES_FETCHING');
 
-export const heroesFetched = (heroes) => {
-  return {
-    type: 'HEROES_FETCHED',
-    payload: heroes,
-  };
-};
+export const heroesFetched = createAction('HEROES_FETCHED');
 
-export const heroesFetchingError = () => {
-  return {
-    type: 'HEROES_FETCHING_ERROR',
-  };
-};
+export const heroesFetchingError = createAction('HEROES_FETCHING_ERROR');
 
-export const heroDeleted = (id) => {
-  return {
-    type: 'HERO_DELETED',
-    payload: id,
-  };
-};
+export const heroDeleted = createAction('HERO_DELETED');
 
-export const heroCreated = (hero) => {
-  return {
-    type: 'HERO_CREATED',
-    payload: hero,
-  };
-};
+export const heroCreated = createAction('HERO_CREATED');
 
 export const filtersFetching = () => {
   return { type: 'FILTERS_FETCHING' };
@@ -58,15 +33,6 @@ export const filtersFetchingError = () => {
     type: 'FILTERS_FETCHING_ERROR',
   };
 };
-
-// export const activeFilterChanged = (filter) => (dispatch) => {
-//   setTimeout(() => {
-//     dispatch({
-//       type: 'ACTIVE_FILTER_CHANGED',
-//       payload: filter,
-//     });
-//   }, 1000);
-// };
 
 export const activeFilterChanged = (filter) => {
   return {
